6b78ebb6d94da4a9c1b41e36a4f4463896a49b90
jeevanv <v.jeevan441@gmail.com>
2023-04-20 09:13:40 +0530

    added jeevan

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>This is heading</h1>
-     <h1>This is git</h1>
+     <h1>Jeevan</h1>
+     
     </div>
   );
 }
