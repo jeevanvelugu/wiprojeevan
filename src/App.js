b1ae08d62a314830e7b2874e04d50cc0259b06b8
jeevanv <v.jeevan441@gmail.com>
2023-04-20 08:15:40 +0530

    added project files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>This is heading</h1>
+     <h1>This is git</h1>
+    </div>
+  );
+}
+
+export default App;
